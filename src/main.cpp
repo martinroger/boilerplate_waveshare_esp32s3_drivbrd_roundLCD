4e53c09f287b4a312ed572e04eb4977713edd248
Martin <martin.roger@me.com>
2024-10-10 15:11:33 +0200

    Upload and Serial comms OK

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,15 @@ int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
   int result = myFunction(2, 3);
+  Serial.println("Setup OK");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println(millis());
+  delay(1000);
 }
 
 // put function definitions here:
